cccdd9f217ba3ccb6f43147f0dd4c92834d86434
nskrdev <kauschik@yahoo.com>
2025-11-23 15:46:55 +0530

    font to jetbrains mono neard

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,19 +1,15 @@
 import {
-  IBM_Plex_Mono as FontMono,
-  IBM_Plex_Sans as FontSans,
+  Geist as FontSans,
+  JetBrains_Mono as FontMono,
   VT323 as FontVT323,
 } from "next/font/google";
 
 export const fontSans = FontSans({
-  weight: ["400", "500", "600"],
-  display: "swap",
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
 export const fontMono = FontMono({
-  weight: ["400", "500", "600"],
-  display: "swap",
   subsets: ["latin"],
   variable: "--font-mono",
 });
